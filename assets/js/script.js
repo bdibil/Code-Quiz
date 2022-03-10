d5560a3c2adb527bdf689a8f9cba143031eec502
Bernardo Dibildox <95992081+bdibil@users.noreply.github.com>
2022-03-09 19:31:33 -0800

    resubmitting code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ const scoresTable = document.getElementById('scores')
 const gameTable = document.getElementById('game')
 const titleEl = document.getElementById('title')
 const startBtn = document.getElementById('start')
+const t1 = document.getElementById('t1')
 
 const currentQEl = document.getElementById('currentQ')
 const ansBtn1 = document.getElementById('ans1')
@@ -10,8 +11,20 @@ const ansBtn3 = document.getElementById('ans3')
 const ansBtn4 = document.getElementById('ans4')
 
 
+const in1 = document.getElementById('in1')
+const in2 = document.getElementById('in2')
+const in3 = document.getElementById('in3')
 
-// const startBtnEl = $('#startBtn');
+const sco1 = document.getElementById('sco1')
+const sco2 = document.getElementById('sco2')
+const sco3 = document.getElementById('sco3')
+
+const submitForm = document.getElementById('submitForm')
+const submitBtn = document.getElementById('subBtn')
+const resBtn = document.getElementById('resBtn')
+const userInitials = document.getElementById('userInitials')
+
+
 
 var startTime = 0;
 var timeLeft = 60;
@@ -21,15 +34,29 @@ var dummy = 10;
 let endTime = 0;
 let score = 0;
 let timeInter = 0;
+let scoresArray = []
+let storedScores = []
 
 
 
 // Constructor function for questions
 function Question(text, choices, correct) {
-    this.text = text;
-    this.choices = choices;
-    this.correct = correct;
+  this.text = text;
+  this.choices = choices;
+  this.correct = correct;
 }
+
+function Highscore(initials, score) {
+  this.initials = initials
+  this.score = score
+}
+
+// let emptyscore = {
+//   initials: "",
+//   score: 0,
+// };
+
+
 
 // Define all questions
 const q01 = new Question('Inside which HTML element do we put the JavaScript?', ['<js>', '<javascript>', '<script>', '<scripting>'], 2)
@@ -49,11 +76,12 @@ const q14 = new Question('What is the correct syntax for referring to an externa
 const q15 = new Question('How does a WHILE loop start?', ['while (i <= 10; i++)', 'while i = 1 to 10', 'while (i <= 10)', 'do while i === i'], 0)
 
 
-const questArray = [q01, q02, q03, q04, q05, q06, q07, q08, q09, q10, q11, q12 , q13, q14, q15]
+const questArray = [q01, q02, q03, q04, q05, q06, q07, q08, q09, q10, q11, q12, q13, q14, q15]
 let usedQs = []
 let newArray = []
 let qsUsed = []
-let qsLeft = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+let highScores = []
+let qsLeft = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 let next = 20
 let stopRandom = false
 
@@ -63,49 +91,65 @@ let correctAnswer = 20
 
 
 // Timer function handler 
-function timeFunction(){
-  
-    if (timeLeft>0) {
-      timeLeft--
-      dummy++
-      // console.log(timeLeft);
-    }
-    timeNow = moment().format("X");
-    timeDelta = timeNow-startTime;
-    $("#t1").text(timeLeft);
+function timeFunction() {
 
-    if (timeLeft===0) {
-      dummy = 0
-      gameOver()
-    }
-    if (usedQs.length>14&&newArray.length<1) {
-      gameOver()
-    }
-
+  if (timeLeft > 0) {
+    timeLeft--
+    dummy++
   }
-  
-  
-  // Starts timer and shows table wih Questions
-  function startGame() {
-    if (startTime===0) {
-    timeInter = setInterval(timeFunction,1000)
+  timeNow = moment().format("X");
+  timeDelta = timeNow - startTime;
+
+  t1.textContent = timeLeft
+
+  if (timeLeft === 0) {
+    dummy = 0
+    gameOver()
+  }
+  if (usedQs.length > 14 && newArray.length < 1) {
+    gameOver()
+  }
+
+}
+
+function readHscores (){
+  storedScores = JSON.parse(localStorage.getItem("storedScores"));
+  if (storedScores !== null) {
+    scoresArray = storedScores
+  } 
+}
+
+function saveScore(initials) {
+  let s = new Highscore (initials, score);
+  scoresArray.push(s)
+  localStorage.setItem("storedScores", JSON.stringify(scoresArray))
+  updateScores()
+}
+
+
+
+// Starts timer and shows table wih Questions
+function startGame() {
+  readHscores()
+  if (startTime === 0) {
+    timeInter = setInterval(timeFunction, 1000)
     startTime = moment().format("X");
     console.log(startTime);
   }
-  if (dummy===0) {
+  if (dummy === 0) {
     startTime = moment().format("X");
     console.log(startTime);
-      timeLeft = 10
-    }
-    gameTable.classList.remove('d-none');
-    titleEl.classList.add('d-none');
-    startBtn.classList.add('d-none');
-    let rand = Math.floor(Math.random()*15)
-    displayQs(rand)
+    timeLeft = 10
   }
-  
-  
-function displayQs(num){
+  gameTable.classList.remove('d-none');
+  titleEl.classList.add('d-none');
+  startBtn.classList.add('d-none');
+  let rand = Math.floor(Math.random() * 15)
+  displayQs(rand)
+}
+
+
+function displayQs(num) {
   let randQ = num
   currentQEl.textContent = questArray[randQ].text
   ansBtn1.textContent = questArray[randQ].choices[0]
@@ -118,102 +162,106 @@ function displayQs(num){
   usedQs.push(randQ)
 }
 
-function randomizer(){
-  let rand = Math.floor(Math.random()*15)
+function randomizer() {
+  let rand = Math.floor(Math.random() * 15)
   return rand
 }
 
-// function tryAgain(){
-//   console.log("I'm here")
-//   nextQ()
-// }
-
-function nextQ (){
-
-  if ( !stopRandom ) {
+function nextQ() {
+  if (!stopRandom) {
     next = randomizer()
-    if(qsLeft.includes(next)&&qsUsed.length<9){
+    if (qsLeft.includes(next) && qsUsed.length < 9) {
       qsUsed.push(next)
     } else {
       for (let i = 0; i < qsLeft.length; i++) {
         if (!qsUsed.includes(i)) {
           newArray.push(i)
           stopRandom = true
-        } 
+        }
       }
     }
-  } 
+  }
   else {
-    if (newArray.length>0) {
+    if (newArray.length > 0) {
       next = newArray.shift()
     }
   }
-displayQs(next)
+  displayQs(next)
 }
 
 
-function gameOver(){
+function updateScores(){
+
+  if (scoresArray !== null) {
+
+    scoresArray.sort((el1, el2) => el2.score - el1.score)
+
+    let initArray = scoresArray.map(el => el.initials)
+    let showScores = scoresArray.map(el => el.score)
+    
+    in1.textContent = initArray[0]
+    in2.textContent = initArray[1]
+    in3.textContent = initArray[2]
+    
+    sco1.textContent = showScores[0]
+    sco2.textContent = showScores[1]
+    sco3.textContent = showScores[2]
+
+  } 
+
+}
+
+
+
+function gameOver() {
   endTime = moment().format("X");
-  console.log("used Qs below")
-  console.log(usedQs)
-  console.log(newArray)
-  score = endTime - startTime
-  score = score * usedQs.length
-  console.log("Total Score")
-  console.log(score)
+
+  score = timeLeft * 2
+  score += usedQs.length * 2
   clearInterval(timeInter)
   gameTable.classList.add('d-none');
   // titleEl.classList.add('d-none');
   // startBtn.classList.remove('d-none');
   scoresTable.classList.remove('d-none')
+  submitForm.classList.remove('d-none')
+  updateScores()
 }
 
 
-function userAns0(){checkAnswer(0)}
-function userAns1(){checkAnswer(1)}
-function userAns2(){checkAnswer(2)}
-function userAns3(){checkAnswer(3)}
+function saveInitials() {
+  let initials = userInitials.value
+  saveScore(initials)
+}
 
 
-function checkAnswer (num){
-  if (correctAnswer!==num&&timeLeft>5) {
-    timeLeft = timeLeft-5
+function userAns0() { checkAnswer(0) }
+function userAns1() { checkAnswer(1) }
+function userAns2() { checkAnswer(2) }
+function userAns3() { checkAnswer(3) }
+
+
+function checkAnswer(num) {
+  if (correctAnswer !== num && timeLeft > 5) {
+    timeLeft = timeLeft - 5
   }
   nextQ()
 }
 
 
-  // Event listeners 
-  startBtn.addEventListener('click', startGame)
-  ansBtn1.addEventListener('click', userAns0)
-  ansBtn2.addEventListener('click', userAns1)
-  ansBtn3.addEventListener('click', userAns2)
-  ansBtn4.addEventListener('click', userAns3)
+// Event listeners 
+startBtn.addEventListener('click', startGame)
+ansBtn1.addEventListener('click', userAns0)
+ansBtn2.addEventListener('click', userAns1)
+ansBtn3.addEventListener('click', userAns2)
+ansBtn4.addEventListener('click', userAns3)
 
 
+submitBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  saveInitials()
+})
 
+resBtn.addEventListener('click', function (event){
+  location.reload()
+})
 
-  // startBtnEl.on('click', function() {
-  //   if (startTime===0) {
-  //     setInterval(timeFunction,1000)
-  //     startTime = moment().format("X");
-  //     console.log(startTime);
-  //   }
-    
-  //   if (dummy===0) {
-  //       startTime = moment().format("X");
-  //       console.log(startTime);
-  //       timeLeft = 10
-  //   }
-  //   // startGame()
-  // });
-  
-  
-  
-
-
-
-
-
-
-  
